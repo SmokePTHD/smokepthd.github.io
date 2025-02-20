df108854d1104ee79ba975b809cee673bfea6f2f
SmokePTHD <glapdesousa@gmail.com>
2025-02-20 09:14:03 +0000

    Fix burger menu 	modified:   assets/CSS/global.css 	modified:   assets/JS/Responsive.js

--- assets/JS/Responsive.js
+++ assets/JS/Responsive.js
@@ -1,5 +1,6 @@
-document.getElementById("burger-menu").addEventListener("click", function() {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("active");
-  });
-  
+document.getElementById("burger-menu").addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+  const body = document.body;
+  menu.classList.toggle("active");
+  body.classList.toggle("active-menu");
+});
